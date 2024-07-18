15831e8b8bc413b953abf04f3bcd26407ab74dc7
arvin <baiwuz66@gmail.com>
2024-07-18 17:13:36 +0800

    trajectory_type

--- src/scout_unitree/scout_unitree_control/include/scout_tracking.h
+++ src/scout_unitree/scout_unitree_control/include/scout_tracking.h
@@ -11,6 +11,9 @@
 #include <cmath>
 #include <Eigen/Cholesky>
 
+typedef Eigen::Vector3d VectorX;
+typedef Eigen::Vector2d VectorU;
+
 class ScoutTracking
 {
 public:
@@ -18,10 +21,14 @@ public:
     void computeControl();
     bool first_send_;
 
+    VectorX diff(const VectorX &state, const VectorU &input) const;
+    void step(VectorX &state, const VectorU &input, const double dt) const;
+
 private:
     void odomCallback(const nav_msgs::Odometry::ConstPtr &msg);
     void goalCallback(const scout_unitree_control::TrajectoryArray::ConstPtr &msg);
     void getDesire(double t, double &v_d, double &w_d, geometry_msgs::Pose &desire_pose);
+    VectorX compensateDelay(const VectorX &x0);
     Eigen::Vector3d getError(const geometry_msgs::Pose &current_pose, const geometry_msgs::Pose &goal_pose);
 
     ros::NodeHandle nh_;
@@ -39,6 +46,37 @@ private:
     double angular_k3_;
     int current_goal_index_;
     ros::Time start_time_;
+
+    std::deque<VectorU> historyInput_;
+    int history_length_;
+    double delay_;
+    double dt_;
 };
+
+inline VectorX ScoutTracking::diff(const VectorX &state, const VectorU &input) const
+{
+    VectorX ds;
+    double phi = state(0);
+    double x = state(1);
+    double y = state(2);
+
+    double v = input(0);
+    double w = input(1);
+
+    ds(0) = w;
+    ds(1) = v * cos(phi);
+    ds(2) = v * sin(phi);
+    return ds;
+}
+
+inline void ScoutTracking::step(VectorX &state, const VectorU &input, const double dt) const
+{
+    // RungeKutta
+    VectorX k1 = diff(state, input);
+    VectorX k2 = diff(state + k1 * dt / 2, input);
+    VectorX k3 = diff(state + k2 * dt / 2, input);
+    VectorX k4 = diff(state + k3 * dt, input);
+    state = state + (k1 + 2 * k2 + 2 * k3 + k4) * dt / 6;
+}
 
 #endif
--- src/scout_unitree/scout_unitree_control/src/scout_tracking.cpp
+++ src/scout_unitree/scout_unitree_control/src/scout_tracking.cpp
@@ -1,15 +1,23 @@
 #include "scout_tracking.h"
 
-ScoutTracking::ScoutTracking(ros::NodeHandle &nh) : nh_(nh), linear_k1_(1.0), angular_k2_(1.0), angular_k3_(1.0), current_goal_index_(0)
+ScoutTracking::ScoutTracking(ros::NodeHandle &nh) : nh_(nh), linear_k1_(1.0), angular_k2_(1.0), angular_k3_(1.0), current_goal_index_(0), delay_(0.2), dt_(0.1)
 {
     odom_sub_ = nh_.subscribe("/odom", 10, &ScoutTracking::odomCallback, this);
     // odom_sub_ = nh_.subscribe("/aft_mapped_to_init", 10, &ScoutTracking::odomCallback, this);
     goal_sub_ = nh_.subscribe("/trajectory", 10, &ScoutTracking::goalCallback, this); // 订阅轨迹话题
     cmd_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
 
-    nh_.param("linear_k1", linear_k1_, linear_k1_);
-    nh_.param("angular_k2", angular_k2_, angular_k2_);
-    nh_.param("angular_k3", angular_k3_, angular_k3_);
+    nh_.param("linear_k1", linear_k1_);
+    nh_.param("angular_k2", angular_k2_);
+    nh_.param("angular_k3", angular_k3_);
+    nh_.param("delay_", delay_);
+    nh_.param("dt_", dt_);
+
+    history_length_ = std::ceil(delay_ / dt_);
+    for (int i = 0; i < history_length_; ++i)
+    {
+        historyInput_.emplace_back(0, 0);
+    }
 
     start_time_ = ros::Time::now();
 }
@@ -66,20 +74,39 @@ void ScoutTracking::getDesire(double t, double &v_d, double &w_d, geometry_msgs:
     desire_pose = trajectory_[idx];
 }
 
+VectorX ScoutTracking::compensateDelay(const VectorX &x0)
+{
+    VectorX x0_delay = x0;
+    for (double t = delay_; t > 0; t -= dt_)
+    {
+        int i = std::ceil(t / dt_);
+        VectorU input = historyInput_[history_length_ - i];
+        step(x0_delay, input, dt_);
+    }
+
+    return x0_delay;
+}
+
 Eigen::Vector3d ScoutTracking::getError(const geometry_msgs::Pose &current_pose, const geometry_msgs::Pose &desire_pose)
 {
+    double phi_c = tf::getYaw(current_pose.orientation);
     double x_c = current_pose.position.x;
     double y_c = current_pose.position.y;
-    double phi_c = tf::getYaw(current_pose.orientation);
+    VectorX x0;
+    x0 << phi_c, x_c, y_c;
+    VectorX x0_delay = this->compensateDelay(x0);
 
+    double phi_d = tf::getYaw(desire_pose.orientation);
     double x_d = desire_pose.position.x;
     double y_d = desire_pose.position.y;
-    double phi_d = tf::getYaw(desire_pose.orientation);
 
     // get error
-    double phi_e = phi_c - phi_d;
-    double x_e = (x_c - x_d) * cos(phi_d) + (y_c - y_d) * sin(phi_d);
-    double y_e = -(x_c - x_d) * sin(phi_d) + (y_c - y_d) * cos(phi_d);
+    // double phi_e = phi_c - phi_d;
+    // double x_e = (x_c - x_d) * cos(phi_d) + (y_c - y_d) * sin(phi_d);
+    // double y_e = -(x_c - x_d) * sin(phi_d) + (y_c - y_d) * cos(phi_d);
+    double phi_e = x0_delay(0) - phi_d;
+    double x_e = (x0_delay(1) - x_d) * cos(phi_d) + (x0_delay(2) - y_d) * sin(phi_d);
+    double y_e = -(x0_delay(1) - x_d) * sin(phi_d) + (x0_delay(2) - y_d) * cos(phi_d);
 
     Eigen::Vector3d q_e = {phi_e, x_e, y_e};
 
@@ -111,16 +138,17 @@ void ScoutTracking::computeControl()
     desire_pose.position.z = 0.0;
     desire_pose.orientation = tf::createQuaternionMsgFromYaw(0.0);
     getDesire(t, v_d, w_d, desire_pose);
-    // v_d = 0.03 * std::pow(t, 2) - 0.006 * std::pow(t, 3) + 0.0003 * std::pow(t, 4);
-    // w_d = 0.0;
-    // v_d = 0.2 * M_PI * 5;
-    // w_d = 0.2 * M_PI;
 
+    std::cout << "v = 1" << std::endl;
     geometry_msgs::Twist cmd;
     Eigen::Vector3d q_error = this->getError(current_pose_, desire_pose);
+    std::cout << "v = 2" << std::endl;
 
     cmd.linear.x = (v_d - linear_k1_ * abs(v_d) * (q_error(1)) + (q_error(2) * tan(q_error(0)))) / cos(q_error(0));
     cmd.angular.z = w_d - (angular_k2_ * v_d * q_error(2) + angular_k3_ * abs(v_d) * tan(q_error(0))) * cos(q_error(0) * cos(q_error(0)));
+    VectorU predicInput = {cmd.linear.x, cmd.angular.z};
+    historyInput_.pop_front();
+    historyInput_.push_back(predicInput);
     std::cout << "v = " << cmd.linear.x << std::endl;
     std::cout << "w = " << cmd.angular.z << std::endl;
 
